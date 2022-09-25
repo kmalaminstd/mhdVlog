f3c7bf1906db7b907dc3c90977165f5e24be729b
kmalamin <alaminkhanstd@gmail.com>
2022-09-25 13:01:01 +0600

    homepage desging complete

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// selectors
+// nav selectors
+
+const navElm = document.querySelector('nav')
+const innerNavElm = document.querySelector('.inner-nav')
+const webHeaderElm = document.querySelector('header')
+
+
